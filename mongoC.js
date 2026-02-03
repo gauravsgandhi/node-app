9d8b8fba4d2470ba6070ef0aeb8c8b4382932f9c
sgauravgandhi <gaurav.s.gandhi@philips.com>
2026-02-03 12:01:06 +0530

    updated few config file

--- mongoC.js
+++ mongoC.js
@@ -6,10 +6,11 @@ const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
 
 const connectionString = `mongodb+srv://gauravsgandhi_db_user:${password}@mern-app.tjd0j8k.mongodb.net/?retryWrites=true&w=majority`; // cluster url
 const client = new MongoClient(connectionString);
+console.log(client);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
+  console.log("connection successful");
 } catch(e) {
   console.error(e);
 }
