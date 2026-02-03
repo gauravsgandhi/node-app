320622a622d3599869372a9588a57cde2ed8085b
sgauravgandhi <gaurav.s.gandhi@philips.com>
2026-02-03 11:24:13 +0530

    Added few config file

--- mongoC.js
+++ mongoC.js
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+
 const connectionString = `mongodb+srv://gauravsgandhi_db_user:${password}@mern-app.tjd0j8k.mongodb.net/?retryWrites=true&w=majority`; // cluster url
 const client = new MongoClient(connectionString);
 let conn;
